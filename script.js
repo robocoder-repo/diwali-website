9ad06358086fbc237d0ca68434f81d5f53990133
Robocoders <team@blackbox.ai>
2024-10-27 15:10:17 +0000

    Update

--- script.js
+++ script.js
@@ -6,12 +6,15 @@ canvas.height = window.innerHeight;
 
 const fireworks = [];
 const particles = [];
+const diyas = [];
+
+let soundEnabled = false;
 
 // Firework class
 class Firework {
-    constructor() {
-        this.x = Math.random() * canvas.width;
-        this.y = canvas.height;
+    constructor(x, y) {
+        this.x = x || Math.random() * canvas.width;
+        this.y = y || canvas.height;
         this.sx = Math.random() * 3 - 1.5;
         this.sy = Math.random() * -3 - 3;
         this.size = 2;
@@ -57,6 +60,32 @@ class Particle {
     }
 }
 
+// Diya class
+class Diya {
+    constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = 5;
+        this.angle = 0;
+    }
+
+    update() {
+        this.angle += 0.02;
+        this.y += Math.sin(this.angle) * 0.5;
+    }
+
+    draw() {
+        ctx.fillStyle = '#FFA500';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.fillStyle = '#FF4500';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size / 2, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
+
 // Animation loop
 function animate() {
     ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
@@ -70,7 +99,7 @@ function animate() {
             createParticles(fireworks[i].x, fireworks[i].y, fireworks[i].color);
             fireworks.splice(i, 1);
             i--;
-            playSound();
+            if (soundEnabled) playSound();
         }
     }
 
@@ -82,6 +111,11 @@ function animate() {
             particles.splice(i, 1);
             i--;
         }
+    }
+
+    for (let diya of diyas) {
+        diya.update();
+        diya.draw();
     }
 
     if (Math.random() < 0.05) {
@@ -103,6 +137,23 @@ const audio = new Audio('https://www.soundjay.com/mechanical/sounds/firework-exp
 function playSound() {
     audio.currentTime = 0;
     audio.play();
+}
+
+// Create fireworks on click
+canvas.addEventListener('click', (e) => {
+    fireworks.push(new Firework(e.clientX, e.clientY));
+});
+
+// Toggle sound
+const soundToggle = document.getElementById('sound-toggle');
+soundToggle.addEventListener('click', () => {
+    soundEnabled = !soundEnabled;
+    soundToggle.textContent = soundEnabled ? '🔊 Sound On' : '🔇 Sound Off';
+});
+
+// Create diyas
+for (let i = 0; i < 10; i++) {
+    diyas.push(new Diya());
 }
 
 // Start animation
